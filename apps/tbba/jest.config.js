ac51d718d1f249e668efa3e4271792c652b7c04b
egocentryk <lukasz.skowron@gmail.com>
2021-03-23 00:50:59 +0100

    initial commit

---
+++ apps/tbba/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'tbba',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/tbba',
+};
